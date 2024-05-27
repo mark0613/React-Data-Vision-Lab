29b95f9f6d66d2fa6a0ee28e8aea70a683a05c56
Mark <goldffd700@gmail.com>
2024-05-27 12:39:02 +0800

    feat: 打包後的靜態檔案路徑

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '',
     server: {
         host: '127.0.0.1',
         port: 3000,
